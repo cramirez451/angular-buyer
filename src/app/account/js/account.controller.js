802788217c84246fd7e940037e55ca851b900626
Crhistian Ramirez <cramirez@four51.com>
2017-06-09 16:27:36 -0500

    this is a worthwhile commit

--- src/app/account/js/account.controller.js
+++ src/app/account/js/account.controller.js
@@ -7,6 +7,10 @@ function AccountController($exceptionHandler, toastr, OrderCloudSDK, ocAccount, 
 	vm.profile = angular.copy(CurrentUser);
 	vm.currentUser = CurrentUser;
 
+	vm.coolFunction = function(){
+		'This is a worthwhile comit';
+	};
+
 	vm.usernameChange = function () {
 		vm.settingsForm.Username.$setValidity('User.UsernameMustBeUnique', true);
 	};
